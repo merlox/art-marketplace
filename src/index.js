b8d358cf7e1771f0d5ea58017fa57317c265f806
merlox <merunasgrincalaitis@gmail.com>
2019-02-18 15:02:55 +0100

    Integrated 0xcert

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { MetamaskProvider } from '@0xcert/ethereum-metamask-provider'
+import { AssetLedger } from '@0xcert/ethereum-asset-ledger'
 
 class Main extends React.Component {
     constructor() {
         super()
+        start()
+    }
+
+    async start() {
+        const provider = new MetamaskProvider()
+        const ledgerId = '0xeF5781A2c04113e29bE5724ae6E30bC287610007'
+        const ledger = new AssetLedger(provider, ledgerId)
+        const balance = await ledger.getBalance('0xeF5781A2c04113e29bE5724ae6E30bC287610007')
+        console.log('balance', balance)
     }
 
     render() {
