a3e05142ad67b120b75e9e54a70dd946af48ae49
merlox <merunasgrincalaitis@gmail.com>
2019-02-18 16:16:55 +0100

    Added createNewAsset and deployNewAssetLedger functions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { MetamaskProvider } from '@0xcert/ethereum-metamask-provider'
 // The Capabilities determine what those mods can do with the assets they are managing
 // The Ethereum address that deploys this ledger has full powers to do whatever he wants as the administrator
 import { AssetLedger, AssetLedgerCapabilities } from '@0xcert/ethereum-asset-ledger'
+import { schema88 } from '@0xcert/conventions'
+import { Cert } from '@0xcert/cert'
 
 class Main extends React.Component {
     constructor() {
@@ -13,7 +15,7 @@ class Main extends React.Component {
         this.state = {
             provider: {}
         }
-        this.start()
+        this.createNewAsset()
     }
 
     async start() {
@@ -25,13 +27,28 @@ class Main extends React.Component {
         console.log('balance', balance)
     }
 
+    // To generate new ERC721 assets
+    async createNewAsset() {
+        const cert = new Cert({
+            schema: schema88
+        })
+        const asset = {
+            description: 'An lighthouse watercolor picture',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Taran_Lighthouse_Kalinigrad_Oblast_Tatiana_Yagunova_Watercolor_painting.jpg',
+            name: 'Lighthouse Watercolor'
+        }
+        console.log('Imprint', await cert.imprint(asset))
+        console.log('Disclose', await cert.disclose(asset, [['name', ['image']]]).then(result => JSON.stringify(result)))
+    }
+
+    // To create a new asset ledger containing several assets and managed by several individuals
     async deployNewAssetLedger() {
         // The required keys are name, symbol, uriBase and schemaId
         const recipe = {
             name: 'Art Piece',
             symbol: 'ART',
             uriBase: '',
-            schemaId: ''
+            schemaId: '',
             capabilities: [
                 AssetLedgerCapabilities.DESTROY_ASSET,
                 AssetLedgerCapabilities.UPDATE_ASSET,
@@ -39,7 +56,6 @@ class Main extends React.Component {
                 AssetLedgerCapabilities.TOGGLE_ASSET
             ]
         }
-
         // This requires a smart contract transaction with gas and such cuz its called a mutation
         const ledgerMutation = await AssetLedger.deploy(this.state.provider, recipe)
     }
